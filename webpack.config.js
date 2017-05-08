0fece4d185e3897536b7e583cf67ebd1f2933d7c
Tommo565 <Ewingt1979@gmail.com>
2017-05-08 10:59:33 +0100

    d3 issues

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,14 @@ var webpack = require('webpack');
 module.exports = {
 
 	plugins: [
-		new webpack.ProvidePlugin({
-			  $: 'jquery',
-			  jQuery: 'jquery',
-			})
+     new webpack.ProvidePlugin({
+            d3: "d3"
+        }),
+
+	new webpack.ProvidePlugin({
+		  $: 'jquery',
+		  jQuery: 'jquery',
+		})
 	],
 
 	entry: "./scripts/App.js",
